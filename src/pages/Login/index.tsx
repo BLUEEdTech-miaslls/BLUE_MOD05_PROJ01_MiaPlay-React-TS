3e317bf0f66fa627f7f7fd2aee518c8bbca6e747
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-01 17:18:49 -0300

    Login: disabled notice + navigate to Home

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,15 @@
 import "./Login.css";
 
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+import { RoutePath } from "../../types/routes";
+
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [showNotice, setShowNotice] = useState<Boolean>(false);
+
   return (
     <>
       <div className="login-outer-container">
@@ -15,26 +24,65 @@ const Login = () => {
               type="text"
               name="email"
               placeholder="email"
-              autoComplete="email"
+              autoComplete="off"
+              onSelect={() => setShowNotice(true)}
             />
             <input
               className="login-input"
               type="password"
               name="password"
               placeholder="password"
-              autoComplete="current-password"
+              autoComplete="off"
+              onSelect={() => setShowNotice(true)}
             />
 
             <div className="login-button-container">
-              <button className="login-button clickable">
+              <button
+                type="button"
+                className="login-button clickable"
+                onClick={() => setShowNotice(true)}
+                onSubmit={(e) => e.preventDefault}
+              >
                 <i className="bi bi-plus-circle"></i>
               </button>
 
-              <button className="login-button clickable">
+              <button
+                type="button"
+                className="login-button clickable"
+                onClick={() => navigate(RoutePath.HOME)}
+              >
                 <i className="bi bi-check-circle"></i>
               </button>
             </div>
           </form>
+
+          {showNotice && (
+            <div className="login-notice">
+              <div className="login-notice-icon">
+                <i className="bi bi-info-circle-fill"></i>
+              </div>
+
+              <div className="wrapper">
+                <div>
+                  login is <b>non-functional.</b>
+                </div>
+                <span> click </span>
+                <span
+                  className="clickable"
+                  onClick={() => navigate(RoutePath.HOME)}
+                >
+                  <i className="bi bi-check-circle"></i>
+                </span>
+                <span> to go </span>
+                <span
+                  className="clickable"
+                  onClick={() => navigate(RoutePath.HOME)}
+                >
+                  <span> HOME </span>
+                </span>
+              </div>
+            </div>
+          )}
         </main>
       </div>
     </>
