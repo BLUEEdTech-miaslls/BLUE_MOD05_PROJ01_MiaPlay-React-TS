e206ee785a299aa192aa99c694c1349cb796aad8
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-01 12:15:09 -0300

    remodel entity interfaces

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -1,17 +1,29 @@
-export interface Genre {
-  id: String;
+export interface GenreBody {
   name: String;
-  games?: Game[];
 }
 
-export interface Game {
+export interface Genre extends GenreBody {
   id: String;
+}
+
+export interface GameBody {
   title: String;
   cover_imgUrl: String;
   year: Number;
   description: String;
-  imdbScore: String;
+  imdbScore: Number;
   trailer_youTubeUrl: String;
   gameplay_youTubeUrl: String;
   genres: Genre[];
+}
+
+export interface Game extends GameBody {
+  id: String;
+  favorite: Boolean;
+}
+
+export interface GenreGameList {
+  id: String;
+  genre: Genre;
+  games: Game[];
 }
