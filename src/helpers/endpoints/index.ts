524700031b22fdfe0a3864dbb7ea7d337d0d2693
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-02 08:41:50 -0300

    fix endpoints typo

--- src/helpers/endpoints/index.ts
+++ src/helpers/endpoints/index.ts
@@ -3,7 +3,7 @@ import { games } from "./games";
 import { genreGameLists } from "./genreGameLists";
 
 export const endpoints = {
-  baseUrl: "http://miaplay-api-01.onrender.com",
+  baseUrl: "https://miaplay-api-01.onrender.com",
   ...genres,
   ...games,
   ...genreGameLists,
