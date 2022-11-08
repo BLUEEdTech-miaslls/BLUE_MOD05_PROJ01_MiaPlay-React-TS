51b1bc72b9d98e1d660114aea45868a9c925f4fb
miaslls <97785569+miaslls@users.noreply.github.com>
2022-11-08 12:13:29 -0300

    include reference

--- src/pages/Admin/GameForm/util/validateGameForm/validateYear.ts
+++ src/pages/Admin/GameForm/util/validateGameForm/validateYear.ts
@@ -1,6 +1,7 @@
 const validateYear = (year: string) => {
   if (year) {
     if (Number(year) < 1958) {
+      // 🔗 https://www.bnl.gov/about/history/firstvideo.php
       return "year invalid";
     }
     if (Number(year) > new Date().getFullYear()) return "year invalid";
